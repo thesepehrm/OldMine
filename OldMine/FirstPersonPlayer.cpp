7f8115658e46963f26828e79fa73ed45fcd3b310
Sepehr Mohammadi <mohamadi.sepehr@live.com>
2017-12-26 13:00:41 +0330

    added collision function

--- OldMine/FirstPersonPlayer.cpp
+++ OldMine/FirstPersonPlayer.cpp
@@ -6,8 +6,10 @@
 //  Copyright © 2017 Mohammadi.io. All rights reserved.
 //
 
-#include <iostream>
 #include "FirstPersonPlayer.h"
+#include <math.h>
+
+// Constructor
 
 FirstPersonPlayer::FirstPersonPlayer()
 {
@@ -15,6 +17,23 @@ FirstPersonPlayer::FirstPersonPlayer()
     this->position = {0,0,0};
     this->center_eye = {0,0,0};
 }
+
+// Player Actions
+bool FirstPersonPlayer::isColliding(Point targetPos, float size_x, float size_y, float size_z)
+{
+    if ( abs(targetPos.x - this->position.x) < (size_x + this->size_x) )
+        if ( abs(targetPos.y - this->position.y) < (size_y + this->size_y) )
+            if ( abs(targetPos.z - this->position.z) < (size_z + this->size_z) )
+                return true;
+    return false;
+}
+
+
+
+
+
+
+// Get and Set functions
 
 void FirstPersonPlayer::setPos(Point pos)
 {
@@ -41,6 +60,13 @@ void FirstPersonPlayer::setJumpHeight(float jump_height)
     this->jump_height = jump_height;
 }
 
+void FirstPersonPlayer::setPlayerSize(int width, int height)
+{
+    this->size_x = width;
+    this->size_y = height;
+}
+
+
 Point FirstPersonPlayer::getPos()
 {
     return this->position;
@@ -65,5 +91,6 @@ float FirstPersonPlayer::getJumpHeight()
 {
     return this->jump_height;
 }
+
 
 
--- OldMine/FirstPersonPlayer.h
+++ OldMine/FirstPersonPlayer.h
@@ -25,21 +25,33 @@ class FirstPersonPlayer
     float speed = 2; // Block per second
     float life = 5; // Hearts
     
+    int size_x = 1; // width of the player
+    int size_y = 2; // height of the player
+    int size_z = 1; // depth of the player
+    
+    
     
 public:
     FirstPersonPlayer();
     
+    // Player actions
+    bool isColliding(Point,float=1,float=1,float=1);
+    
+    
+    // Get and Set functions
     void setPos(Point);
     void setEyeCenter(Point);
     void setJumpHeight(float);
     void setLife(float);
     void setSpeed(float);
+    void setPlayerSize(int,int);
     
     Point getPos();
     Point getEyeCenter();
     float getJumpHeight();
     float getLife();
     float getSpeed();
+    
     
     
 };
